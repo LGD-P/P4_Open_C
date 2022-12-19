e7e640c20899c8928493e3db1ef8fabbc492c177
LGD-P <nplgdjp@gmx.com>
2022-12-20 00:29:31 +0100

    save changes

--- bidouille_swizz_logic.py
+++ bidouille_swizz_logic.py
@@ -41,16 +41,27 @@ second_rank_part = player_rank_index_list[4:9]
 first_list_of_match = []
 for element_1, element_2 in zip(first_rank_part,second_rank_part):
     first_list_of_match.append([element_1, element_2])
+    TOURNAMENT_LIST[0].tours.append([element_1, element_2])
 
     
 # Ainsi il n'y a plus qu'a aller chercher l'index du joueur dans la liste
 # Correspondante pour faire l'annonce des matchs.
 
 
+
+
 counter = -1
+listing_of_match_first_round = []
 for element in first_list_of_match:
     counter += 1
     print(f"{(PLAYERS_LIST[first_list_of_match[counter][0][1]].last_name)} joue contre " \
         f"{(PLAYERS_LIST[first_list_of_match[counter][1][1]].last_name)}")
 
+    listing_of_match_first_round.append(
+        ([(PLAYERS_LIST[first_list_of_match[counter][0][1]].last_name),
+         (PLAYERS_LIST[first_list_of_match[counter][0][1]].rank)],
+        [(PLAYERS_LIST[first_list_of_match[counter][1][1]].last_name),
+         (PLAYERS_LIST[first_list_of_match[counter][1][1]].rank)]))
 
+for element in listing_of_match_first_round:
+    print(tuple(element))
--- test_controller.py
+++ test_controller.py
@@ -11,7 +11,7 @@ PLAYERS_LIST = [
                 Players("DENIS", "Laurent", "11-12-2000","h",321,0),
                 Players("LAURENT", "Denis", "11-10-2005","h",123,0),
                 Players("MOINE", "Alice", "10-10-1990","f",100,0),
-                Players("VAULT", "Lise", "01-02-1980","F",10,0),
+                Players("VAULT", "Lise", "01-02-1980","f",10,0),
                 Players("CREPIN", "Maurice", "12-07-1950","h",40,0),
                 Players("TIAGO", "Daniela", "05-06-1977","f",35,0),
                 Players("EDON", "Gabrielle", "09-03-1985","f",25,0),
