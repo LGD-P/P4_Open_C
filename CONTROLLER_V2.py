d5546a7daa94c41452ce11ce2d999985812d6eec
LGD-P <nplgdjp@gmx.com>
2022-12-26 16:48:36 +0100

    fixe add_player in specific tournament

--- CONTROLLER_V2.py
+++ CONTROLLER_V2.py
@@ -17,8 +17,12 @@ TOURNAMENT_LIST = [Tournament("Chess-Event","Paris",datetime.now().strftime("%d-
                                   Players("VAULT", "Lise", "01-02-1980","f",10,0),
                                   Players("CREPIN", "Maurice", "12-07-1950","h",40,0),
                                   Players("TIAGO", "Daniela", "05-06-1977","f",35,0),
-                                  Players("EDON", "Gabrielle", "09-03-1985","f",25,0)
-                                  ],"Blitz","Description",4)] 
+                                  Players("EDON", "Gabrielle", "09-03-1985","f",25,0),
+                                  Players("PATTON", "Gabriel", "09-03-1970","h",20,0)
+                                  ],"Blitz","Description",4), 
+                   Tournament("Chess-Show","Moscow",datetime.now().strftime("%d-%m-%Y"),
+                              [],[],"Blitz","Description",4)
+                   ]] 
 
 @dataclass
 class MenuController:
@@ -41,7 +45,8 @@ class TournamentController:
         counter = 0
         for tournois in TOURNAMENT_LIST:
             counter +=1
-            c.print(f"\nTounois N°{counter} : {tournois.name}")
+            c.print(f"\nTounois N°{counter} : {tournois.name}\n"
+                    f"\n Joueur : {tournois.players} ")
             
             
     def add_tournament(self):
@@ -97,77 +102,136 @@ class PlayerController:
     
     
     def add_player(self):
-        last_name= c.input("[bold green3]Entrez le nom du Joueur: [bold green3] ")
-        while last_name.isdigit():
-            c.print("[bold red]\nInvalide, le nom ne peut pas contenir "\
-                    "de numéro\n ")
-            last_name= c.input("[bold green3]Entrez le nom du Joueur:"\
-                "[bold green3] ")
-                    
+        if not TOURNAMENT_LIST:
+            c.print("[bold red] Il vous faut d'abord créer un tournois "\
+                "pour pouvoir y ajouter des joueurs.[bold red]")
             
-        first_name= c.input("[bold green3]Entrez le prénom du Joueur: "\
-            "[bold green3] ")
-        while first_name.isdigit():
-            c.print("[bold red]\nInvalide, le prénom ne peut pas contenir "\
-                    "de numéro\n ")
-            first_name = c.input("[bold green3]Entrez le prénom du Joueur:"\
-                "[bold green3] ")
-
-        c.print("[bold green3]Entrez la date de naissance du joueur :")
-        
-        day = c.input("Jour: ")
-        while not day.isdigit() or int(day) not in range(1,32):
-            c.print("[bold red]\nInvalide: entrez un jour entre 1 et 31")
-            day = c.input("Jour: ")
-        
-        month = c.input("Mois: ")
-        while not month.isdigit() or int(month) not in range(1,13):
-            c.print("[bold red]\nInvalide: entrez un jour entre 1 et 12")
-            month = c.input("Mois: ")
-
+            
+        else:             
+            c.print(f"\n[bold yellow]Liste des tournois disponible :\n\n[bold yellow]")
+            tournament_list_choice = []
+            for tournois in TOURNAMENT_LIST:
+                if not len(tournois.players) == 8:
+                    tournament_list_choice.append(TOURNAMENT_LIST.index(tournois) + 1)
+                    c.print(f"[bold blue]- N°{TOURNAMENT_LIST.index(tournois) + 1} :"
+                            f" {tournois.name}\n[bold blue]")
                 
-        while int(month) == 2 and int(day) in range(29,32):
-            c.print("[bold red] Le mois de février ne compte pas de 29 30 "\
-            "ou 31[bold red]")
+            tournament_choice = c.input("[bold yellow]Entrez le N° du tournois"\
+            " dans lequel vous souhaitez ajouter des joueurs. \n[bold yellow]")
+            
+
+            while not int(tournament_choice) in tournament_list_choice:
+                tournament_choice = c.input("[bold red]Entrez le N° d'un"\
+            " tournois proposé dans la liste. \n[bold red]")
+            
+            """
+            try:
+                TOURNAMENT_LIST[int(tournament_choice)]
+            except IndexError:
+                tournament_choice = c.input("[bold red]Entrez le N° d'un"\
+                " tournois existant. \n[bold red]")
+            """
+                    
+            result = TOURNAMENT_LIST[int(tournament_choice) - 1]
+            
+          
+            last_name= c.input("[bold green3]Entrez le nom du Joueur: [bold green3] ")
+            while last_name.isdigit():
+                c.print("[bold red]\nInvalide, le nom ne peut pas contenir "\
+                        "de numéro\n ")
+                last_name= c.input("[bold green3]Entrez le nom du Joueur:"\
+                    "[bold green3] ")
+                        
+                
+            first_name= c.input("[bold green3]Entrez le prénom du Joueur: "\
+                "[bold green3] ")
+            while first_name.isdigit():
+                c.print("[bold red]\nInvalide, le prénom ne peut pas contenir "\
+                        "de numéro\n ")
+                first_name = c.input("[bold green3]Entrez le prénom du Joueur:"\
+                    "[bold green3] ")
+
+            c.print("[bold green3]Entrez la date de naissance du joueur :")
             
             day = c.input("Jour: ")
             while not day.isdigit() or int(day) not in range(1,32):
                 c.print("[bold red]\nInvalide: entrez un jour entre 1 et 31")
                 day = c.input("Jour: ")
-                
+            
             month = c.input("Mois: ")
             while not month.isdigit() or int(month) not in range(1,13):
                 c.print("[bold red]\nInvalide: entrez un jour entre 1 et 12")
                 month = c.input("Mois: ")
-                    
-        year = c.input("Année: ")
-        while not year.isdigit() or not int(year) in range(
-            (int(datetime.now().strftime("%Y"))-118),
-            (int(datetime.now().strftime("%Y"))-10)) :
-                c.print("[bold red]\nInvalide: Le joueur doit avoir "\
-                    "au moins 10 ans, au plus 118 ans")
-                year = c.input("Année: ")
-        
-        birth= (f'{day}-{month}-{year}')
 
-        sex_list = ["h","f"]
-        sex= c.input("[bold green3]Entrez le sexe: H - F[bold green3] ")
-        while not sex.lower() in sex_list or sex.isdigit():
-            c.print("[bold red]\nInvalide ")
-            sex = c.input("[bold green3]Entrez le sexe du joueur: H / F "\
-                "[bold green3]")
-            
-        rank= c.input("[bold green3]Entrez le classement du joueur:" \
-            "[bold green3]")
+                    
+            while int(month) == 2 and int(day) in range(29,32):
+                c.print("[bold red] Le mois de février ne compte pas de 29 30 "\
+                "ou 31[bold red]")
                 
-        added_player = Players(last_name,first_name,str(birth),sex,int(rank),0)
-        
-        TOURNAMENT_LIST[0].players.append(added_player)
+                day = c.input("Jour: ")
+                while not day.isdigit() or int(day) not in range(1,32):
+                    c.print("[bold red]\nInvalide: entrez un jour entre 1 et 31")
+                    day = c.input("Jour: ")
+                    
+                month = c.input("Mois: ")
+                while not month.isdigit() or int(month) not in range(1,13):
+                    c.print("[bold red]\nInvalide: entrez un jour entre 1 et 12")
+                    month = c.input("Mois: ")
+                        
+            year = c.input("Année: ")
+            while not year.isdigit() or not int(year) in range(
+                (int(datetime.now().strftime("%Y"))-118),
+                (int(datetime.now().strftime("%Y"))-10)) :
+                    c.print("[bold red]\nInvalide: Le joueur doit avoir "\
+                        "au moins 10 ans, au plus 118 ans")
+                    year = c.input("Année: ")
+            
+            birth= (f'{day}-{month}-{year}')
+
+            sex_list = ["h","f"]
+            sex= c.input("[bold green3]Entrez le sexe: H - F[bold green3] ")
+            while not sex.lower() in sex_list or sex.isdigit():
+                c.print("[bold red]\nInvalide ")
+                sex = c.input("[bold green3]Entrez le sexe du joueur: H / F "\
+                    "[bold green3]")
+                
+            rank= c.input("[bold green3]Entrez le classement du joueur:" \
+                "[bold green3]")
+                    
+            added_player = Players(last_name,first_name,str(birth),sex,int(rank),0)
+            
+            result.players.append(added_player)
 
 
     def display_player_listing(self):
         for element in TOURNAMENT_LIST:
             c.print(element.players)
+        
+        
+"""        
+      
+    def select_tournament_for_player(self):
+        counter = 0
+        c.print(f"\n[bold yellow]Tournois disponible(s):\n\n[bold yellow]")
+        for tournois in TOURNAMENT_LIST:
+            if len(tournois.players) < 8:
+            counter += 1
+            c.print(f"[bold blue]- {counter} : {tournois.name}\n[bold blue]")
+            
+        tournament_choice = c.input("[bold yellow]Entrez le N° du tournois"\
+            " dans lequel vous souhaitez ajouter des joueurs. \n[bold yellow]")
+            
+        try:
+            TOURNAMENT_LIST[int(tournament_choice)]
+        except IndexError:
+            tournament_choice = c.input("[bold red]Entrez le N° d'un"\
+            " tournois existant. \n[bold red]")
+                
+        result = TOURNAMENT_LIST[int(tournament_choice) - 1]
+        return result
+            
+"""           
+            
         
         
 PLAYER_CONTROLLER = PlayerController("")
