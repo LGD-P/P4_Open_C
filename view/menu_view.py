4a81a6c30b17e756328e79a6e1eebe5254ea3141
LGD-P <nplgdjp@gmx.com>
2023-03-07 00:55:53 +0100

    readme

--- view/menu_view.py
+++ view/menu_view.py
@@ -15,6 +15,22 @@ class MenuView:
         Returns:
             function: function choosen by user choice
         """
+        c.print("""[bold green3]
+        ::
+       !##7
+       ?&@?               !7
+     7YB&@#B?          ..^G#~.:            .:
+     :P@@@@#^         .?G&@@@#J:           5B.         .J5?!~:
+      !&@@@!            ^B@@@!           ~G&7PY.       .?&@@@&7J.        . . . ..
+     .?&&@@J:           ^#&@@!          ^P@#P@@?      !B&&@@@@B@!!      .JG&G&BG:
+    .!G&&&@#?.         !P&&&@#?.         ~B@@&Y:    ^G@@@@@&@@@&BG       ?@@@@@G          .^:
+      :B@@#:            ^B@@#!           ~G&&&?.    ^YB@5!^!&@@@#?7      7G#&@&J         !B@@P
+       5@@B              Y@@G            ^5@@#!.          :5@@@&#&Y       ~&@@7          ^#@@Y
+      ^Y@@@~            .Y@@&:            ~&@5          ~P&@@@@&P7.       ^B@@7         .7#&@P^
+    :7!P@@@&?:        .~!Y@@@B~.          !#@#.        Y@@@@@@@#BG^      .?G@@#:          J@&^
+   :G&#&@@@@@#^       Y#B&@@@@@G.       ~YY&@@#?.       B@@@@@@&5~      :5Y&@@@#~        :J@@J
+  :?#&&&&&@@@@Y^    .!B&@&&@@@@&?.     :B@@@@@@@7      :G&&@@@@@?       J&@@@@@@P.      5##@@@&
+ :PGGGGBB##&@@@&:   JPGGBBB#&&@@@B.   !PGBB##&@@@5    ~5GBB##&@@@5    .YGBBB#&&@@B^   .YGB##&@@&?[bold green3]""")
         c.print("\n[bold yellow] :clipboard: CHESS MENU Veuillez faire un "
                 "choix dans le menu: :clipboard: [bold yellow]\n")
         for element in self.main_menu_view:
